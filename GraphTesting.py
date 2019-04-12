144966533fda48ecbb865b227a29e82b8395236c
James <jamessteven301@gmail.com>
2019-04-12 11:33:20 -0400

    GraphTesting Changes

--- GraphTesting.py
+++ GraphTesting.py
@@ -8,11 +8,16 @@ y = []
 
 df = pandas.read_csv('recentNOBuff.csv')
 
-x=df["t"]
+x=df["t"] - 1554753711.83
 y=df["r"]
-
+y2= 1.6 * pow((1.0 + 0.75*(1.6/-0.4)*x),(1.0/.75))
+print(x)
+print(y2)
 plt.plot(x,y, label='Range over Time')
+plt.plot(x,y2, label='EF')
 plt.xlabel('t')
 plt.ylabel('r')
 plt.title('Range over Time')
-plt.savefig("RvTPlot")
+plt.grid()
+plt.legend()
+plt.savefig("Test")
