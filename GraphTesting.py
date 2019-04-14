e7fb1c0860ede1b41700673040c345e308efcdb2
James <jamessteven301@gmail.com>
2019-04-13 20:02:35 -0400

    Factored GraphTesting

--- GraphTesting.py
+++ GraphTesting.py
@@ -3,34 +3,31 @@ import csv
 import pandas
 import datetime
 
-# Plotting data with MatPlotLib
-x = []
-y = []
-currentTime = datetime.datetime.now()
+# Plots flight graph from data in 'filename' csv, saves to FlightGraphs folder
 
-# Get most recent csv
-df = pandas.read_csv('recentNOBuff.csv')
+def flightgraph (filename, v0, tau_dot, r0):
+    # Plotting data with MatPlotLib
+    x = []
+    y = []
+    currentTime = datetime.datetime.now()
 
+    # Get most recent csv
+    df = pandas.read_csv(filename)
 
-# Initialize variables
-v0 = -0.4
-tau_dot = 0.5
-r0 = 1.6
+    # Get flight data and ideal calculations
+    x=df["t"]-1554753711.83
+    y=df["r"]
+    yIdeal = r0 * pow((1+((tau_dot*v0*x)/r0)), 1/tau_dot)
 
-# Get ideal data
-x=df["t"]-1554753711.83
-y=df["r"]
-yIdeal = r0 * pow((1+((tau_dot*v0*x)/r0)), 1/tau_dot)
+    # Plot both lines
+    plt.plot(x,y, label='Flight Data')
+    plt.plot(x,yIdeal, label='Ideal Flight with EF')
+    plt.legend()
+    plt.grid()
+    plt.xlabel('Time (s)')
+    plt.ylabel('Range (m)')
+    plt.title('Range vs. Time')
 
-# Plot both lines
-plt.plot(x,y, label='Flight Data')
-plt.plot(x,yIdeal, label='Ideal Flight with EF')
-plt.legend()
-plt.grid()
-plt.xlabel('Time (s)')
-plt.ylabel('Range (m)')
-plt.title('Range vs. Time')
-
-# Save file to FlightGraphs folder
-fileName = "{}{}".format("FlightGraphs/FlightGraph_", currentTime.strftime("%Y-%m-%d-%H:%M"))
-plt.savefig(fileName)
+    # Save file to FlightGraphs folder
+    fileName = "{}{}".format("FlightGraphs/FlightGraph_", currentTime.strftime("%Y-%m-%d-%H:%M"))
+    plt.savefig(fileName)
